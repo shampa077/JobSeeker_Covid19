d221d628146d14717a0b90a5533622bc0822f43e
Shampa Shahriyar <shampa077@gmail.com>
2020-08-22 23:03:36 +1000

    heroku deployment test

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"shampa077@gmail.com\"\n\
+echo "[general]
+email = \"shampa077@gmail.com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
